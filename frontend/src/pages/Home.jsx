ae32e3213418fe0f19c6003016d2780d683b3a1e
Antonio <antoniovalent111@gmail.com>
2025-06-21 11:37:22 +0200

    Add: addded hero section and two fonts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
-const home = () => {
+const Home = () => {
   return (
     <div>
-      
+      <Hero />
     </div>
   )
 }
 
-export default home
+export default Home
